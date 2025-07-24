b1a271f0df1701e70827a4c029ddc50074ec73f3
teeranate <teeranate_tho@cmu.ac.th>
2025-07-24 21:38:30 +0700

    Lab 3 5.3 Network error view

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,7 @@ import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -62,6 +63,12 @@ const router = createRouter({
       component: NotFoundView
 
      },
+     {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView
+
+   },
     {
       path: '/students',
       name: 'student-list-view',
