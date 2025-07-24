526ce9a08dd0689c1c58a638d18ec8c2c36f1650
teeranate <teeranate_tho@cmu.ac.th>
2025-07-24 21:33:33 +0700

    Lab 3 5-5.1 error page

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import EventDetailView from '@/views/event/DetailView.vue'
 import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -49,6 +50,18 @@ const router = createRouter({
       name: 'about',
       component: AboutView
     },
+    {
+      path: '/404/:resource',
+      name: '404-resource-view',
+      component: NotFoundView,
+      props: true
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
+
+     },
     {
       path: '/students',
       name: 'student-list-view',
