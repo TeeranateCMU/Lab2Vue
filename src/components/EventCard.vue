cc28cdf4efaa276a6e778b5aecb743b9f0495947
teeranate <teeranate_tho@cmu.ac.th>
2025-07-24 16:13:09 +0700

    Lab 3 1.5

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -18,10 +18,10 @@ defineProps<{
 </script>
 
 <template>
-  <div class="event-card">
+  <RouterLink to="/event/5928101">
     <h2>{{ event.title }}</h2>
     <span>{{ event.time }} on {{ event.date }}</span>
-  </div>
+  </RouterLink>
 </template>
 <style scoped>
 .event-card {
